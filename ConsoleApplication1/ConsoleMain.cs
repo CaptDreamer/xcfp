c3d1a4ce1f70363f4a9c69bfe5ad8aa1c0843edc
Amit Bader <CaptDreamer@users.noreply.github.com>
2016-05-06 20:14:19 -0500

    Initial Commit

--- ConsoleApplication1/ConsoleMain.cs
+++ ConsoleApplication1/ConsoleMain.cs
@@ -1,25 +1,23 @@
 ﻿using System;
+using X2CFP;
 
-namespace X2CFP
+public class ConsoleMain
 {
-    public class ConsoleMain
+    public static int Main(string[] args)
     {
-        public static int Main(string[] args)
+        if (args.Length != 1)
         {
-            if (args.Length != 1)
-            {
-                Console.WriteLine("Usage: X2CFP.exe character_file");
-                return 1;
-            }
-            
-            CharacterPool pool = new CharacterPool(args[0]);
-            foreach(Character c in pool.Characters())
-            {
-                Console.WriteLine(c.ToString());
-                Console.WriteLine(c.Details());
-            }
-            return 0;       
+            Console.WriteLine("Usage: X2CFP.exe character_file");
+            return 1;
         }
+            
+        CharacterPool pool = new CharacterPool(args[0]);
+        foreach(Character c in pool.Characters())
+        {
+            Console.WriteLine(c.ToString());
+            Console.WriteLine(c.Details());
+        }
+        return 0;       
     }
 }
     
